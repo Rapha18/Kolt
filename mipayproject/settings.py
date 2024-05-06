cc3619b93b13d0c1ea037b47f496d3c4d34acc87
Rapha18 <rgnorima@gmail.com>
2024-05-06 17:10:39 +0000

    second commit

--- mipayproject/settings.py
+++ mipayproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^-!@0c2a3buh-5f-7dtl-zmyxg8aw4@ea$1@ftf0#d99f^$=3w'
+SECRET_KEY = config(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['77.37.87.220','.mipay-africa.com']
 
 
 # Application definition
